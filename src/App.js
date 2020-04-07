c6740a6eb8d95304cdb21c4169c740662cc60b3d
Dubenis <dubenis85201@gmail.com>
2020-04-07 15:50:26 -0500

    repo creation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css';
+
+
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
