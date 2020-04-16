28f13a4e9361d1798481d63e0eca20737b06f197
Julieth <juliethquinchia@gmail.com>
2020-04-15 21:37:28 -0500

    Footer principal

--- src/component/Registro.js
+++ src/component/Registro.js
@@ -1,48 +1,53 @@
-import React, { Component,useState,Fragment } from 'react';
+import React, { Component, useState, Fragment } from 'react';
 import Logo from '../Imagenes/logo.png'
 import axios from 'axios';
+import emailjs from 'emailjs-com';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
-  } from "react-router-dom";
-  import '../index.css'
+} from "react-router-dom";
+import '../index.css'
 
-    
+
 
 class Registro extends Component {
-    state={
-        form:{
-            nombres:'',
-            apellidos:'',
-            correo:'',
-            contrasena:0,
-            ConfirmacionContrasena:0,
-            numeroCedula:0
-        }  
+    state = {
+        form: {
+            nombres: '',
+            apellidos: '',
+            correo: '',
+            contrasena: 0,
+            ConfirmacionContrasena: 0,
+            numeroCedula: 0,           
+          
+        }
     };
-    handleChange=(event)=>{
+    handleChange = (event) => {
         this.setState({
-            form:{
+            form: {
                 ...this.state.form,
-                [event.target.name]:event.target.value
+                [event.target.name]: event.target.value
             }
         })
     }
-    
+
     handleSubmit = event => {
-        event.preventDefault();     
-        axios.post(`https://datosregistro.now.sh/datosUsuario/`, {...this.state.form})
-          .then(res => {
-           if(res!=null){
-            localStorage.setItem('user', JSON.stringify(res.data)); //local storage de la data para asi llevarlos al home
-            this.props.history.push('/home');
-            }
-          })
-        
-      }
-    render(){
+        event.preventDefault();
+        axios.post(`https://datosregistro.now.sh/datosUsuario/`, { ...this.state.form })
+            .then(res => {
+                if (res != null) {
+                    localStorage.setItem('user', JSON.stringify(res.data)); //local storage de la data para asi llevarlos al home
+                    this.props.history.push('/home');
+                }
+            })      
+            
+
+            
+
+    }
+    render() {
         return (
             <Fragment>
                 <header>
@@ -54,139 +59,139 @@ class Registro extends Component {
                     </nav>
 
                 </header>
-               
-            
-            
-            <div className="container-fluid d-flex justify-content-center align-item-center body-registro " >
-              
-                <form className="form-signin" onSubmit={this.handleSubmit}
-                >
-                    <div className="text-center mb-4">
-                       
-                        <img className="mb-4 mt-4" src={Logo} alt="" width="72" height="72"></img>
-                        <h1 className="h3 mb-3 font-weight-normal text-center text-white">Registro</h1>
 
-                    </div>
-                    <div className="form-row">
-                        <div className="col mb-3">
-                            <label className="text-white " for="validationServer01">Nombres</label>
-                            <input
-                               type="text"
-                                name="nombres"
-                                placeholder="Ingrese nombres"
-                                onChange={this.handleChange}
-                                className="form-control "
-                                 ></input>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
+
+
+                <div className="container-fluid d-flex justify-content-center align-item-center body-registro " >
+
+                    <form className="form-signin" onSubmit={this.handleSubmit}
+                    >
+                        <div className="text-center mb-4">
+
+                            <img className="mb-4 mt-4" src={Logo} alt="" width="72" height="72"></img>
+                            <h1 className="h3 mb-3 font-weight-normal text-center text-white">Registro</h1>
+
                         </div>
-
-                        <div className="col mb-3">
-                            <label  className="text-white " for="validationServer02">Apellidos</label>
-                            <input  className="form-control"
-                                type="text"
-                                name="apellidos"
-                           
-                                placeholder="Ingrese Apellidos"
-                           
-                                onChange={this.handleChange}
+                        <div className="form-row">
+                            <div className="col mb-3">
+                                <label className="text-white " for="validationServer01">Nombres</label>
+                                <input
+                                    type="text"
+                                    name="nombres"
+                                    placeholder="Ingrese nombres"
+                                    onChange={this.handleChange}
+                                    className="form-control "
                                 ></input>
-                             <div className="valid-feedback">
-                                Looks good!
+                                <div className="valid-feedback">
+                                    Looks good!
                             </div>
-                        </div>
+                            </div>
 
+                            <div className="col mb-3">
+                                <label className="text-white " for="validationServer02">Apellidos</label>
+                                <input className="form-control"
+                                    type="text"
+                                    name="apellidos"
 
+                                    placeholder="Ingrese Apellidos"
 
-                    </div>
-                    <div className="form-row">
-                        <div className="col mb-3">
-                            <label className="text-white " for="validationServer02">Correo electrónico</label>
-                            <input
-                               className="form-control "
-                                type="email"
-                                name="correo"
-                             
-                                placeholder="Ingrese correo electronico"
-                              
-                                onChange={this.handleChange}
+                                    onChange={this.handleChange}
                                 ></input>
-                            <div className="valid-feedback">
-                                Looks good!
+                                <div className="valid-feedback">
+                                    Looks good!
+                            </div>
                             </div>
 
 
-                        </div>
-
-                        <div className="col mb-3">
-                            <label className="text-white " for="validationServer02">Numero de cedula</label>
-                            <input  className="form-control "
-                                type="text" 
-                                name="numeroCedula"
-                             
-                                placeholder="Numero de cedula"
-                                onChange={this.handleChange}
-                              ></input>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
 
                         </div>
+                        <div className="form-row">
+                            <div className="col mb-3">
+                                <label className="text-white " for="validationServer02">Correo electrónico</label>
+                                <input
+                                    className="form-control "
+                                    type="email"
+                                    name="correo"
 
+                                    placeholder="Ingrese correo electronico"
 
-                    </div>
-                    <div className="form-row">
-                        <div className="col mb-3">
-                            <label  className="text-white " for="validationServer02">Contraseña</label>
-                            <input  className="form-control "
-                                type="password"
-                                name="contrasena"
-                             
-                                placeholder="Ingrese contraseña"
-                                onChange={this.handleChange}
-                             
-                                 ></input>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
-
-
-                        </div>
-
-                        <div className="col mb-3">
-                            <label className="text-white " for="validationServer02">Confirmación de contraseña</label>
-                            <input className="form-control "
-                                type="password"
-                                name="ConfirmacionContrasena"
-                                placeholder="Ingrese contraseña"
-                                onChange={this.handleChange}
-                                
+                                    onChange={this.handleChange}
                                 ></input>
-                            <div className="valid-feedback">
-                                Looks good!
+                                <div className="valid-feedback">
+                                    Looks good!
+                            </div>
+
+
+                            </div>
+
+                            <div className="col mb-3">
+                                <label className="text-white " for="validationServer02">Numero de cedula</label>
+                                <input className="form-control "
+                                    type="text"
+                                    name="numeroCedula"
+
+                                    placeholder="Numero de cedula"
+                                    onChange={this.handleChange}
+                                ></input>
+                                <div className="valid-feedback">
+                                    Looks good!
+                            </div>
+
+                            </div>
+
+
+                        </div>
+                        <div className="form-row">
+                            <div className="col mb-3">
+                                <label className="text-white " for="validationServer02">Contraseña</label>
+                                <input className="form-control "
+                                    type="password"
+                                    name="contrasena"
+
+                                    placeholder="Ingrese contraseña"
+                                    onChange={this.handleChange}
+
+                                ></input>
+                                <div className="valid-feedback">
+                                    Looks good!
+                            </div>
+
+
+                            </div>
+
+                            <div className="col mb-3">
+                                <label className="text-white " for="validationServer02">Confirmación de contraseña</label>
+                                <input className="form-control "
+                                    type="password"
+                                    name="ConfirmacionContrasena"
+                                    placeholder="Ingrese contraseña"
+                                    onChange={this.handleChange}
+
+                                ></input>
+                                <div className="valid-feedback">
+                                    Looks good!
+                            </div>
+
                             </div>
 
                         </div>
 
-                    </div>
-                 
-                        <button className="btn btn-lg btn-primary rounded-pil  btn-block" type="submit" 
-                        style={{
-                            backgroundColor:'#0C927D' 
-                        }}>Registrarse</button>
+                        <button className="btn btn-lg btn-primary rounded-pil  btn-block" type="submit"
+                            style={{
+                                backgroundColor: '#0C927D'
+                            }}>Registrarse</button>
 
-                  
-                   
-                    <p className="mt-5 mb-3 text-muted text-center text-white">© 2020</p>
-                </form>
-            </div>
-            <footer className="footer-registro"></footer>
+
+
+                        <p className="mt-5 mb-3 text-muted text-center text-white">© 2020</p>
+                    </form>
+                </div>
+                <footer className="footer-registro"></footer>
 
 
             </Fragment>
-           
-            
+
+
 
         );
     }
