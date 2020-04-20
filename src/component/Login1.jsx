e4fc6f9ec271ab3a5feb5e93219c429f6f4dcc09
Dubenis <dubenis85201@gmail.com>
2020-04-20 16:24:43 -0500

    bye white line

--- src/component/Login1.jsx
+++ src/component/Login1.jsx
@@ -4,6 +4,16 @@ import logo  from '../images/logo.png';
 import { Link, Redirect} from 'react-router-dom';
 import Login from './Login';
 import axios from 'axios';
+import Img1 from '../Imagenes/Img1.png'
+import Img2 from '../Imagenes/Img2.png';
+import Img3 from '../Imagenes/Img3.png';
+import facebook from '../Imagenes/facebook.png';
+import twitter from '../Imagenes/twitter.png';
+import inst from '../Imagenes/inst.png';
+import phone from '../Imagenes/icons8-home-26.png';
+import home from '../Imagenes/icons8-phone-24.png';
+import email from '../Imagenes/email-icon.png';
+import '../App.css'
 
 class Login1 extends Component {
   
@@ -117,9 +127,67 @@ class Login1 extends Component {
                     <Login/>      
                 </form>
             </div>
+            <footer
+                        className="footer">
+                        <div className="row justify-content-center footer-one p-3 text-header text-white" >
+                            By Team ECOPAY
+                        <i>                            
+                            <a href="https://www.facebook.com/ecopaygeek" target="_blank">
+                                <img className="mx-2" width="30" height="auto" src={facebook} alt="" />
+                            </a>
+                        </i>
+                            <i><img className="mx-2" width="30" height="auto" src={twitter} alt="" /></i>
+                            <i><img className="mx-2" width="30" height="auto" src={inst} alt="" /></i>
+                        </div>
 
+                        <div className="container">
+                            <div className="row justify-content-center">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-sm-8">
+                                            <h5 id="textfooter" className="texto-header text-uppercase text-responsive">Nuestros Aliados</h5>
+                                            <ul className="listado">
+                                                <li>
+                                                    <i><img className="img-responsive mx-2" width="50" src={Img1} alt="" /></i>
+                                                    Agile Innova
+                                                </li>
+                                                <li>
+                                                    <i><img className="img-responsive mx-2" width="50" src={Img2} alt="" /></i>
+                                                    Makaia
+                                                </li>
 
+                                                <li>
+                                                    <i><img className="img-responsive mx-2" width="50" src={Img3} alt="" /></i>
+                                                    Academia Geek
+                                                </li>
+                                            </ul>
 
+                                        </div>
+
+                                        <div className="col-sm-4 ">
+                                            <h5 id="textfooter" className="texto-header text-uppercase text-responsive justify-content-center">Contactanos</h5>
+                                            <ul className="listado justify-content-left">
+                                                <li>
+                                                    <i><img className="img-responsive mx-2" width="20" src={home} alt="" /></i>
+                                                    +573115991986
+
+                                                </li>
+                                                <li>
+                                                    <i><img className="img-responsive mx-2" width="20" src={phone} alt="" /></i>
+                                                Medellín, Colombia
+                                                </li>
+                                                <li>
+                                                    <i><img className="img-responsive mx-2" width="20" src={email} alt="" /></i>
+                                                    ecopay@gmail.com
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
       </>
     );
   }
